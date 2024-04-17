063fda8db567df5ce440f66174958dfb2e567f39
sloor <antant8085@gmail.com>
2024-04-17 11:40:08 -0400

    updated settings

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ class Settings:
         self.fleet_direction = 1 #1 represents right and -1 represents left
 
         # How Quickly the game soeeds up
-        self.speedup_scale = 1.2
+        self.speedup_scale = 1.1
         
         # How quickly the alien point values increase
         self.score_scale = 1.5
